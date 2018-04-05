72167011e36d920ac4881fb5ce043a9310a993b8
Vitaly Greck <vintozver@ya.ru>
2018-04-05 01:42:15 -0700

    generic centralized storage - sqlite

--- pi_registrar/server.py
+++ pi_registrar/server.py
@@ -3,26 +3,34 @@ import ipaddress
 import cherrypy
 import http
 import datetime
+import dateutil.relativedelta
 import cryptography.x509
 import cryptography.hazmat.backends
-from threading import Lock
+import sqlite3
 
 
 class Store(object):
-    def __init__(self):
-        self.lock = Lock()
-
-        self.clients = list()
-        # map client_id to
-        # {ip_addr: {'cert': str, 'dt': utc_dt}
-        self.ip_to_cert = dict()
-        # {cert: {'ip': str, 'dt': utc_dt}
-        self.cert_to_ip = dict()
+    DB = 'hostreg.db'
 
     def hit(self, ip_addr: typing.Union[ipaddress.IPv4Address, ipaddress.IPv6Address], cert: str):
         dt = datetime.datetime.utcnow()
-        self.ip_to_cert[ip_addr] = {'cert': cert, 'dt': dt}
-        self.cert_to_ip[cert] = {'ip': ip_addr, 'dt': dt}
+
+        with sqlite3.connect(self.DB) as db_connection:
+            db_cursor = db_connection.cursor()
+            db_cursor.execute(
+                ''' INSERT INTO maps (address, cert, dt) VALUES (?, ?, ?)''',
+                (str(ip_addr), cert, dt)
+            )
+            db_cursor.execute(
+                ''' DELETE FROM maps WHERE dt < ?''',
+                (dt + dateutil.relativedelta.relativedelta(days=-1), )
+            )
+
+    def read(self):
+        with sqlite3.connect(self.DB) as db_connection:
+            db_cursor = db_connection.cursor()
+            for row in db_cursor.execute('SELECT * FROM maps'):
+                yield str(row)
 
 
 _store = Store()
@@ -32,13 +40,7 @@ class Root(object):
     def _get(self):
         cherrypy.response.headers['Content-Type'] = 'text/plain; encoding=utf-8'
         cherrypy.response.status = '200 OK'
-        yield 'IP to certificate mappings\n'
-        for ip, cert_info in _store.ip_to_cert.items():
-            yield '%48s | %128s | %s' % (
-                ip,
-                ', '.join([subj_item.value for subj_item in cert_info['cert'].subject]),
-                cert_info['dt'].isoformat()
-            )
+        return _store.read()
 
     def _post(self):
         real_ip = ipaddress.ip_address(cherrypy.request.headers['X-Real-IP'])
@@ -46,7 +48,7 @@ class Root(object):
             cherrypy.request.headers['X-SSL-Client-Certificate'].encode('ascii'),
             cryptography.hazmat.backends.default_backend()
         )
-        _store.hit(real_ip, client_crt)
+        _store.hit(real_ip, ', '.join([subj_item.value for subj_item in client_crt.subject]))
         cherrypy.response.headers['Content-Type'] = 'text/plain; encoding=utf-8'
         cherrypy.response.status = '202 Accepted'
         yield 'Your IP: %s\nYour certificate: %s' % (
@@ -65,6 +67,18 @@ class Root(object):
                 http.HTTPStatus.METHOD_NOT_ALLOWED.value, http.HTTPStatus.METHOD_NOT_ALLOWED.phrase
             )
     _post.index = {'response.stream': True}
+
+
+with sqlite3.connect(Store.DB) as db_connection:
+    db_cursor = db_connection.cursor()
+    try:
+        db_cursor.execute(
+            '''
+CREATE TABLE maps (address VARCHAR, cert TEXT, dt DATETIME, PRIMARY KEY (address, cert))
+'''
+        )
+    except sqlite3.OperationalError as err:
+        print(err)
 
 
 cherrypy.config.update({'engine.autoreload.on': False})
